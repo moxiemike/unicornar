1383ab60f69f53e03a72c3bf9d0f47ec8547e460
weirdestnerd <olu1995@hotmail.com>
2017-12-04 18:35:50 -0700

    v 0.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3030
 
 // app settings
 app.set('view engine', 'pug')
